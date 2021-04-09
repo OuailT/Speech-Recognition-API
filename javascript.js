0fa3b3fb78315268d8e995ed71d5bde793920ba3
tayarthouail <tayarthouail@gmail.com>
2021-04-09 14:47:28 +0200

    Conversation with THE APIS

--- javascript.js
+++ javascript.js
@@ -14,41 +14,55 @@ recognition.start();
 // Capture what the user speaks
 function onSpeak(e) {
     const msg = e.results[0][0].transcript;
-    writeMessage(msg);
+    
+    // CheckCode(msg);
+    TalkTheTalk(msg);
 }
 
-//write User speaks
-function writeMessage(msg) {
-    MessageEl.innerHTML = `<div>You said :</div> <span class="box">${msg}</span>`;
-}
 
 //Check message Against text
-function CheckNumber(msg) {
+function CheckCode(msg) {
     const num = msg;
-
-    //Check if Valid Number
-    if(Number.isNaN(num)) {
-        MessageEl.innerHTML = '<div>That is not a valid number</div>';
+    
+    //Check if The code is Valid 
+    if(num == 333 ) {
+        MessageEl.innerHTML = `<div>${msg} Code is Right, How are you Today Ouail!</div>`;
         return;
     }
 
-
-
-
-
-
+     else {
+        MessageEl.innerHTML = `<div>${msg} Code is Wrong, Please Speak The Right Code To talk to me</div>`;
+        return;
+     }
 
 }
 
 
+// Talking with the APIS functions
+function TalkTheTalk(msg) {
 
-//Generate a Random number
-function getRandomNum () {
-    return Math.floor(Math.random()* 100) + 1;
+    const talk = msg; 
+
+    const greetings = ["I'm good", "I'm fine", "I'm Ok"];
+
+     const result = greetings.find((greeting)=> {
+         return greeting === talk;
+        });
+
+        if(result) {
+            MessageEl.innerHTML = '<div> Im so happy to hear that! What can i do for you today ! </div>';
+        }
+        else {
+            MessageEl.innerHTML = '<div>Sorry Ouail I didnt hear !Would you Repeat</div>';
+            
+        }    
+    
 }
+
+
+
 
 //Event Listener 
-
 //Speak result
 recognition.addEventListener('result', onSpeak);
 
